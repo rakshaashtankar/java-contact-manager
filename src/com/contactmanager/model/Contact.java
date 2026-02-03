315aa4bf703bca327f022cdce0021303f95b9220
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2026-02-03 18:02:04 +0530

    Add ContactService skeleton

--- src/com/contactmanager/model/Contact.java
+++ src/com/contactmanager/model/Contact.java
@@ -1,11 +1,13 @@
 package com.contactmanager.model;
 
 public class Contact {
+	
 	private int id;
 	private String name;
 	private String phone;
 	private String email;
 	
+	//Constructor
 	public Contact(int id, String name, String phone, String email) {
 		this.id = id;
 		this.name = name;
@@ -13,6 +15,7 @@ public class Contact {
 		this.email = email;
 	}
 	
+	//Getters and Setters
 	public int getId() {
 		return this.id;
 	}
--- src/com/contactmanager/service/ContactService.java
+++ src/com/contactmanager/service/ContactService.java
@@ -1,5 +1,27 @@
 package com.contactmanager.service;
 
-public class ContactService {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.contactmanager.model.Contact;
+
+public class ContactService {
+	
+	private ArrayList<Contact> contacts = new ArrayList<>();
+	
+	public void addContact(Contact contact) {
+		
+	}
+	
+	public List<Contact> getAllContacts(){
+		return contacts;
+	}
+	
+	public Contact searchByName(String name) {
+		return null;
+	}
+	
+	public boolean deleteById(int id) {
+		return false;
+	}
 }
