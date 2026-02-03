c42a709d0f6b29506e8b4e888952ff3186f026da
Raksha Ashtankar <rakshaashtankar@Rakshas-MacBook-Air.local>
2026-02-03 17:54:48 +0530

    Add Contact model class

--- src/com/contactmanager/model/Contact.java
+++ src/com/contactmanager/model/Contact.java
@@ -1,5 +1,48 @@
 package com.contactmanager.model;
 
 public class Contact {
+	private int id;
+	private String name;
+	private String phone;
+	private String email;
+	
+	public Contact(int id, String name, String phone, String email) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhone() {
+		return this.phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
